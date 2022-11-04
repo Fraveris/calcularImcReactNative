7ff83cc48640ccdd8b6a4bacc603c9cb6514b7b0
Fraveris <101026755+Fraveris@users.noreply.github.com>
2022-11-04 14:46:16 -0300

    Update ResultImc.js

--- components/ResultImc.js
+++ components/ResultImc.js
@@ -1,10 +1,28 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity, Share } from "react-native"
 import ResultImccss from "./ResultImccss"
 
 export default function ResultImc(props) {
-    return(
+
+  const onShare = async () => {
+    const result = await Share.share({
+      message: "Meu imc hoje é: " +props.resultImc,
+    })
+  }
+
+  return(
         <View style={ResultImccss.resultImc}>
+          <View style={ResultImccss.boxShareButton}>
+            {props.resultImc != null ?
+            <TouchableOpacity
+            onPress={onShare}
+            style={ResultImccss.shared}>
+              <Text style={ResultImccss.sharedText}>Share</Text>
+            </TouchableOpacity>
+            :
+            <View/>
+            }
+            </View>
         <Text style={ResultImccss.information}>{props.messageResultImc}</Text> 
           <Text style={ResultImccss.numberImc}>{props.resultImc}</Text>
         </View>
