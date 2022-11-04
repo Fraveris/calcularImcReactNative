d3d4ab6badade9f88b0183d3fa59ed962b28015e
Fraveris <101026755+Fraveris@users.noreply.github.com>
2022-11-04 14:45:47 -0300

    Update ResultImccss.js

--- components/ResultImccss.js
+++ components/ResultImccss.js
@@ -3,15 +3,15 @@ import { StyleSheet } from 'react-native'
 const ResultImccss = StyleSheet.create({
     resultImc: {
         flex: 1,
-        marginTop: 15,
-        paddingTop: 5,
+        marginTop: 20,
+        paddingTop: 15,
         borderRadius: 50,
         alignItems: "center",
         width: "100%",
     },
     numberImc: {
         color: "#FF0043",
-        fontSize: 45,
+        fontSize: 48,
         fontWeight: "bold",
     },
     information: {
@@ -19,6 +19,22 @@ const ResultImccss = StyleSheet.create({
         color: "#FF0043",
         fontWeight: "bold",
     },
+    boxShareButton: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    shared: {
+        backgroundColor: "#1877f2",
+        borderRadius: 50,
+        paddingTop: 5,
+        paddingBottom: 5,
+    },
+    sharedText: {
+        color: "#ffffff",
+        fontWeight: "bold",
+        paddingHorizontal: 30,
+    }
 })
 
 export default ResultImccss
